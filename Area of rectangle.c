82ad5063e50a5d6e0b1ba47cf3f448cd1844654c
Inaam Farooq <inaamzaki1@gmail.com>
2026-05-03 15:53:08 +0530

    Add files via upload

---
+++ Area of rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,area;
+    printf("Enter length:");
+    scanf("%d",&a);
+    printf("Enter breadth:");
+    scanf("%d",&b);
+    area=a*b;
+    printf("The area of rectangle is:%d",area);
+    return 0;
+}
