dda9b056017f7b9217175a5759ede2fa70488e0d
Inaam Farooq <inaamzaki1@gmail.com>
2026-05-03 16:03:45 +0530

    Add files via upload

---
+++ Swap 2 numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter a=");
+    scanf("%d",&a);
+    printf("Enter b=");
+    scanf("%d",&b);
+    c=a;
+    a=b;
+    b=c;
+    printf("After swapping a=%d\n",a);
+    printf("After swapping b=%d\n",b);
+    return 0;
+}
