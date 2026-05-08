03247c2700e1d0052e175fec2ce51c79a7390a85
Inaam Farooq <inaamzaki1@gmail.com>
2026-05-08 15:58:06 +0530

    Add files via upload

---
+++ Delete last element of array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter index:");
+    scanf("%d",&a);
+    int arr[a];
+    printf("Enter the elements of an array:");
+    for(int i=0;i<a;i++){
+    scanf("%d",&arr[i]);
+    }
+    a--;
+    printf("New array:");
+    for(int i=0;i<a;i++){
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
