9369876cba056e756977037093faed22dd3e52c4
Inaam Farooq <inaamzaki1@gmail.com>
2026-06-26 13:57:15 +0530

    Add files via upload

---
+++ swapwithoutusingthirdvariable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the first number:");
+    scanf("%d",&a);
+    printf("Enter the second number:");
+    scanf("%d",&b);
+    //swap
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("After swapping:a=%d and b=%d",a,b);
+    return 0;
+}
